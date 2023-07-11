8d1e01da2dc775b947da4fbdb2b53d200a35c759
cvon117 <125157011+Craigien@users.noreply.github.com>
2023-07-10 21:34:42 -0400

    Setup GET routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const database = require('./db/db.json');
+
 const PORT = 3001;
 
 const app = express();
@@ -15,12 +17,22 @@ app.get('/', (req, res) =>
 );
 
 // -------------- Routes ---------------------
-app.get('/*', (req, res) => {
-    res.send(`Default GET Route and method received`)
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// });
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    return res.json(database)
+
+    // const requestBody = req.body;
+    // console.log(`
+    // ------------------ Request Body ------------------`);
+    // console.log(requestBody);
+    // res.json(requestBody);
 });
 
 app.post('/api/notes', (req, res) => {
